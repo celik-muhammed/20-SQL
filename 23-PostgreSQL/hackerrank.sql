3542ad3554486d9e9724c3195cadf01b267431fc
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2022-12-08 12:40:38 +0300

    Update hackerrank.sql

--- 23-PostgreSQL/hackerrank.sql
+++ 23-PostgreSQL/hackerrank.sql
@@ -5,3 +5,13 @@ where city like '%[a,e,i,o,u]'
 select distinct city
 from station
 where city like '[a,e,i,o,u]%' and city like '%[a,e,i,o,u]' 
+
+
+select 
+    case
+    when A+B<=C or B+C<=A or A+C<=B then "Not A Triangle"
+    when A=B and A=C then "Equilateral"
+    when A=B or A=C or B=C  then "Isosceles"
+    else "Scalene"
+    end
+from triangles;

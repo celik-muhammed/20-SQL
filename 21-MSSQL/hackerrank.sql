4448680ad5451eecf51aa0287c4bbc4be53e17b9
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2022-12-12 20:28:35 +0300

    Update hackerrank.sql

--- 21-MSSQL/hackerrank.sql
+++ 21-MSSQL/hackerrank.sql
@@ -54,3 +54,21 @@ SELECT N,
     END
 FROM bst
 ORDER BY 1;
+
+
+--New Companies
+SELECT company_code, founder,
+    (SELECT COUNT(DISTINCT lead_manager_code) 
+     FROM Lead_Manager 
+     WHERE company_code=c.company_code),
+    (SELECT COUNT(DISTINCT senior_manager_code) 
+     FROM Senior_Manager 
+     WHERE company_code=c.company_code),
+    (SELECT COUNT(DISTINCT manager_code) 
+     FROM Manager 
+     WHERE company_code=c.company_code),
+    (SELECT COUNT(DISTINCT employee_code) 
+     FROM Employee 
+     WHERE company_code=c.company_code)
+FROM Company c
+ORDER BY 1;

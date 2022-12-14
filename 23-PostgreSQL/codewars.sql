175c03bbac41575f93d72ccba4e3df608d416527
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2022-12-14 20:43:26 +0300

    Update codewars.sql

--- 23-PostgreSQL/codewars.sql
+++ 23-PostgreSQL/codewars.sql
@@ -1,5 +1,4 @@
--- https://www.codewars.com/kata/5811315e04adbbdb5000050e/train/sql
--- Create your SELECT statement here
+-- SQL Basics: Simple NULL handling
 SELECT 
 id,
 COALESCE(NULLIF(name, ''), '[product name not found]') as name,
@@ -9,3 +8,18 @@ card_number,
 transaction_date
 FROM  eusales
 WHERE price IS NOT NULL AND price >=50;
+
+
+--Piano Kata, Part 1
+SELECT n,
+  CASE (((n - 1) % 88) % 12)
+    WHEN 0 THEN 'white'
+    WHEN 2 THEN 'white'
+    WHEN 3 THEN 'white'
+    WHEN 5 THEN 'white'
+    WHEN 7 THEN 'white'
+    WHEN 8 THEN 'white'
+    WHEN 10 THEN 'white'
+    ELSE 'black'
+  END AS res
+FROM pianokeys;
